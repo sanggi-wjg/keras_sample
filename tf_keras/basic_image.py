f56fb1f5f58bc40ddc854b3de0edc3654f689747
jay_g <jay_g@kr.accommate.com>
2020-02-04 15:12:12 +0900

    소스 정리

--- tf_keras/basic_image.py
+++ tf_keras/basic_image.py
@@ -31,15 +31,15 @@ def show_mnist_sample_2():
 
 
 if __name__ == '__main__':
-    model = keras.Sequential([
-        keras.layers.Flatten(input_shape = (28, 28,)),
-        keras.layers.Dense(128, activation = 'relu'),
-        keras.layers.Dense(10, activation = 'softmax'),
-    ])
+    # model = keras.Sequential([
+    #     keras.layers.Flatten(input_shape = (28, 28,)),
+    #     keras.layers.Dense(128, activation = 'relu'),
+    #     keras.layers.Dense(10, activation = 'softmax'),
+    # ])
 
-    model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
-    test_loss, test_acc = model.evaluate(test_images, test_labels, verbose = 2)
-    model.fit(train_images, train_labels, epochs = 5)
+    # model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
+    # test_loss, test_acc = model.evaluate(test_images, test_labels, verbose = 2)
+    # model.fit(train_images, train_labels, epochs = 5)
     show_mnist_sample_2()
-    predictions = model.predict(test_images)
-    np.argmax(predictions[0])
+    # predictions = model.predict(test_images)
+    # np.argmax(predictions[0])
